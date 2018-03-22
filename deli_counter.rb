7c79ecd5d1ced432aba95a1c707b63822774fb34
Michelle Blevins <mjblevins7@gmail.com>
2018-03-22 03:09:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(current_line)
+  if current_line.length == 0
+    "The line is currently empty."
+  else
+    puts "The line is currently:"
+    current_line.each_with_index.map do |customer, index|
+      "#{index}. #{customer},"
+end
+
+def take_a_number
+  
