bd148c6421a76c7df3d9f95b190803966987279e
Michelle Blevins <mjblevins7@gmail.com>
2018-03-22 03:13:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(current_line)
   else
     line_right_now = []
     current_line.each_with_index do |customer, index|
-      line_right_now << "#{index+1}. #{customer},"
+      line_right_now << "#{index+1}. #{customer}"
     end
     puts "The line is currently: #{line_right_now.join(" ")}."
   end
